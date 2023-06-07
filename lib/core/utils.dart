d3241a72546670a958f8b59e229dee408e61859d
JuliusKanneh <juliuskanneh7@gmail.com>
2023-06-07 09:00:09 +0300

    adding multiple images to the tweet screen

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar({
   required BuildContext context,
@@ -13,4 +16,17 @@ void showSnackBar({
 
 String getUsernameFromEmail(String email) {
   return email.split('@')[0];
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+
+  ImagePicker imagePicker = ImagePicker();
+  final imageFiles = await imagePicker.pickMultiImage();
+  if (imageFiles.isNotEmpty) {
+    for (final image in imageFiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
 }
--- lib/features/tweet/views/create_tweet_page.dart
+++ lib/features/tweet/views/create_tweet_page.dart
@@ -1,8 +1,12 @@
-import 'dart:developer';
+import 'dart:io';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone_v1/common/common.dart';
+import 'package:twitter_clone_v1/constants/asset_constants.dart';
+import 'package:twitter_clone_v1/core/utils.dart';
 import 'package:twitter_clone_v1/features/auth/controllers/auth_controller.dart';
 import 'package:twitter_clone_v1/theme/pallete.dart';
 
@@ -17,6 +21,20 @@ class TweetView extends ConsumerStatefulWidget {
 }
 
 class _TweetViewState extends ConsumerState<TweetView> {
+  List<File> images = [];
+  TextEditingController tweetTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    tweetTextController.dispose();
+    super.dispose();
+  }
+
+  void onPickImages() async {
+    images = await pickImages();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
@@ -26,7 +44,9 @@ class _TweetViewState extends ConsumerState<TweetView> {
         : Scaffold(
             appBar: AppBar(
               leading: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 icon: const Icon(
                   Icons.close,
                   size: 30,
@@ -41,22 +61,96 @@ class _TweetViewState extends ConsumerState<TweetView> {
                 ),
               ],
             ),
-            body: const SafeArea(
-                child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          "",
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            currentUser.profilePic,
+                          ),
+                          radius: 30,
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: tweetTextController,
+                            decoration: const InputDecoration(
+                              hintText: "What's happening?",
+                              hintStyle: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                            style: const TextStyle(
+                              color: Pallete.greyColor,
+                              fontSize: 22,
+                            ),
+                            maxLines: null,
+                          ),
+                        ),
+                      ],
+                    ),
+                    if (images.isNotEmpty)
+                      CarouselSlider(
+                        items: images.map((file) {
+                          return Container(
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 5,
+                            ),
+                            width: MediaQuery.of(context).size.width,
+                            child: Image.file(file),
+                          );
+                        }).toList(),
+                        options: CarouselOptions(
+                          height: 400,
+                          enableInfiniteScroll: false,
                         ),
                       ),
-                    ],
-                  )
+                  ],
+                ),
+              ),
+            ),
+            bottomNavigationBar: Container(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(width: 0.3, color: Pallete.greyColor),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0).copyWith(
+                      right: 15,
+                      left: 15,
+                    ),
+                    child: GestureDetector(
+                        onTap: onPickImages,
+                        child: SvgPicture.asset(AssetsConstants.galleryIcon)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0).copyWith(
+                      right: 15,
+                      left: 15,
+                    ),
+                    child: SvgPicture.asset(AssetsConstants.gifIcon),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0).copyWith(
+                      right: 15,
+                      left: 15,
+                    ),
+                    child: SvgPicture.asset(AssetsConstants.emojiIcon),
+                  ),
                 ],
               ),
-            )),
+            ),
           );
   }
 }
